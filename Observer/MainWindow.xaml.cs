4ccb83124a9f3b9477aabbebff4c3c9887ba8039
Sadi_er59 <Sadi_er59@itstep.edu.az>
2021-08-14 07:30:30 -0700

    Add project files.

---
+++ Observer/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Observer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OrderLstBx.Items.Add(ProductTextBox.Text);
+        }
+
+        private void NotifyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SendMail.MainWindow = this;
+            SendMail.SendMailMessage(EmailTextBox.Text);
+            MessageBox.Show("Sent!");
+        }
+    }
+}
---
+++ Observer/SendMail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public class SendMail
+    {
+        public static MainWindow MainWindow { get; set; }
+
+        public static void SendMailMessage(string name)
+        {
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("hhhmmmstepit@gmail.com", "0775966907m"),
+                EnableSsl = true,
+            };
+
+            smtpClient.Send("hhhmmmstepit@gmail.com", name, "Information", $"Salam.Yeni {MainWindow.ProductTextBox.Text} geldi.");
+        }
+    }
+}
